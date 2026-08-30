803d60a8564e8357f23262a2789c70cbe81d4049
Developer <developer@example.com>
2026-08-31 02:59:35 +0700

    Update next.config.js and verify all production routes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
   reactStrictMode: true,
   images: {
     domains: ['images.unsplash.com', 'api.dicebear.com', 'img.vietqr.io'],
