2f396cb04fc98064137c65f7b0a04cd53cdfceab
Developer <developer@example.com>
2026-08-31 02:54:36 +0700

    Add Dockerfile, Docker Compose, and GitHub Actions CI/CD Pipeline

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   reactStrictMode: true,
   images: {
     domains: ['images.unsplash.com', 'api.dicebear.com', 'img.vietqr.io'],
